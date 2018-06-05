ade1ea088950a2e37fb97ea8ee85d8b411f6d158
MatthewPlemmons <matthewleeplemmons@gmail.com>
2018-06-05 16:51:55 +0000

    set struct members to null

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -1,5 +1,13 @@
 #include "rb_trees.h"
 
+/**
+ * rb_tree_node - Creates a node for a Red-black tree.
+ *
+ * @parent: parent of node
+ * @value: value of node
+ * @color: color of node
+ * Return: Pointer to node or NULL.
+ */
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 	rb_tree_t *node;
@@ -11,5 +19,7 @@ rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 	node->parent = parent;
 	node->n = value;
 	node->color = color;
+	node->left = NULL;
+	node->right = NULL;
 	return (node);
 }
